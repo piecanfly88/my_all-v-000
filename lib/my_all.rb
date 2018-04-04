d3e0b50d8e72c50bea29b6ac62d4411cb2af3a7d
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-04-04 08:05:56 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 def my_all?(collection)
-
+  my_collection = []
+  i = 0
+  while i < collection.length
+    my_collection << yield(collection[i])
+    i += 1
+  end
+  my_collection.all?
 end
+
+binding.pry
